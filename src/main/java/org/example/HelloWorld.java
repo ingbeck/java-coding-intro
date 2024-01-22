73ac29578ab2965fba6508f2b178265acf3affa4
ingobecker <mail@ingobecker.de>
2024-01-22 16:16:18 +0100

    2.0

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -4,17 +4,28 @@ import java.util.function.ToDoubleBiFunction;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+//1 Java Intro
+//Step 1.1: Create a new class with the name 'HelloWorld'
 public class HelloWorld {
 
+    //Step 1.2: Define the main method in your class.
     public static void main(String[] args) {
 
+        //Step 1.3 - 1.6:
         System.out.println("Java is cool!");
 
-        String a = "a";
-        String b = "b";
-        String sum = a + b;
+        //2 Coding: Operators
+        //Step 2.1: Declare two variables 'a' and 'b' of type int and assign them any integer values.
+        int a = 1;
+        int b = 2;
 
+        //Step 2.2: Add the values of 'a' and 'b' and store the result in a new variable 'sum'.
+        int sum = a + b;
+
+        //Step 2.3: Print the value of 'sum' to the console, along with the text 'Sum: '.
         System.out.println("Sum: " + sum);
+
 
         int zahlInt1 = 52;
         int zahlInt2 = 19;
@@ -30,6 +41,9 @@ public class HelloWorld {
         System.out.println(zahlInt1 + " * " + zahlInt2 + " = " + (zahlInt1 * zahlInt2));
         System.out.println(zahlInt1 + " / " + zahlInt2 + " = " + (zahlInt1 / zahlInt2));
         System.out.println(zahlInt1 + " % " + zahlInt2 + " = " + (zahlInt1 % zahlInt2));
+        System.out.println(zahlInt1 + " > " + zahlInt2 + " = " + (zahlInt1 > zahlInt2));
+        System.out.println(zahlInt1 + " < " + zahlInt2 + " = " + (zahlInt1 < zahlInt2));
+        System.out.println(zahlInt1 + " == " + zahlInt2 + " = " + (zahlInt1 == zahlInt2));
 
         System.out.println("\nDouble:");
         System.out.println("----------");
